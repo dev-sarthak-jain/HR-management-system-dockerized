f0c14efa2e1616a27dd4c5a5ea717180b097c66e
Sarthak Jain <sarthak.jain7213@gmail.com>
2024-04-27 18:48:15 +0530

    Add LLM

--- frontend/app.py
+++ frontend/app.py
@@ -1,33 +1,39 @@
 import streamlit as st
 import pandas as pd
 import requests
-import json
 
 # Function to send JSON to API and get predictions
-def send_json(data, url="http://127.0.0.1:8000/api/attrition_prediction/"):
+def send_json(data, url="http://127.0.0.1:8000/api/employee_create/"):
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data, headers=headers)
-    print(response)
+    response = requests.post(url, json=data, headers=headers)
     return response.json()
 
 def app():
     st.title('Employee Attrition Prediction App')
     
     # File uploader widget
-    uploaded_file = st.file_uploader("Choose a CSV file", type=['csv'])
+    uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type=['csv'])
+    st.text_input("enter","uggyf")
+
     if uploaded_file is not None:
         # Reading the uploaded CSV file
         df = pd.read_csv(uploaded_file)
         
         # Converting each row to JSON and sending to API
-        results = []
-        for _, row in df.iterrows():
-            json_data = row.to_json()
-            result = send_json(json_data)
-            results.append(result)
+        json_data = df.to_dict(orient='records')  # Convert DataFrame to list of dicts
+        results = send_json(json_data)
+
         # Display results
-        for result in results:
-            st.json(result)
+        if results:
+            # Convert results back to DataFrame to display in a table format
+            results_df = pd.DataFrame(results)
+            # Transforming prediction output
+            print(results_df['attrition prediction'])
+            results_df['attrition prediction'] = results_df['attrition prediction'].apply(lambda x: 'High Chances' if x[0] == 1 else 'Low Chances')
+            
+            st.dataframe(results_df)  # Displaying the modified DataFrame
+        else:
+            st.write("No results to display.")
 
 if __name__ == "__main__":
     app()
